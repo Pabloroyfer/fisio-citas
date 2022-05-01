98727f670dcb1686ae926e18478da3f8b3a09841
pabloroyfer <pabloroyfer@gmail.com>
2022-05-01 02:33:28 +0200

    Responsive

--- interactividad.js
+++ interactividad.js
@@ -1,7 +1,18 @@
-/*** BARRA DE MENU */
+/*** BARRA DE MENU ***/
+
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", ()=>{
     navMenu.classList.toggle("nav-menu_visible");
 });
+
+/*** MEDIDAS COVID ***/
+
+const protocolo = document.querySelector('.protocolo');
+const desplegable = document.querySelector('.desplegable');
+console.log(protocolo, desplegable);
+
+protocolo.addEventListener("click",() => {
+    desplegable.style.display = "block";
+})
