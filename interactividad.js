99c87f64c4f8ccc8ad9b2c6effa2bf7d368b8d3c
Pablo Rodríguez <pabloroyfer@gmail.com>
2022-05-04 19:59:48 +0200

    Javascript Inicio de Sesion

--- interactividad.js
+++ interactividad.js
@@ -1,3 +1,40 @@
+/*** SESION ***/
+
+const cierreSesion = document.querySelector(".cierreSesion");
+const inicio = document.querySelector(".inicio");
+const desplegableSesion = document.querySelector(".desplegableSesion");
+const sesion = document.querySelector(".sesion")
+const nuevo = document.querySelector(".nuevo");
+const registro = document.querySelector(".registro");
+
+console.log(inicio, desplegableSesion, nuevo, cierreSesion, sesion);
+
+inicio.addEventListener("click", ()=>{
+    desplegableSesion.style.display="flex";
+    desplegableSesion.style.height="200px";
+    sesion.style.display="flex";
+    if(registro.style.display="flex"){
+        registro.style.display="none";
+    }        
+});
+nuevo.addEventListener("click", ()=>{
+    desplegableSesion.style.display="flex";
+    desplegableSesion.style.height="250px";
+    registro.style.display="flex";
+    if(sesion.style.display="flex"){
+        sesion.style.display="none";
+    }
+});
+cierreSesion.addEventListener("click",()=>{
+    if(registro.style.display="flex"){
+        registro.style.display="none";
+    }
+    if(sesion.style.display="flex"){
+        sesion.style.display="none";
+    }
+    desplegableSesion.style.display = "none";
+})
+
 /*** BARRA DE MENU ***/
 
 const navToggle = document.querySelector(".nav-toggle");
@@ -10,9 +47,13 @@ navToggle.addEventListener("click", ()=>{
 /*** MEDIDAS COVID ***/
 
 const protocolo = document.querySelector('.protocolo');
-const desplegable = document.querySelector('.desplegable');
-console.log(protocolo, desplegable);
+const desplegable = document.querySelector('.desplegableCovid');
+const cierreCovid = document.querySelector(".cierreCovid");
+console.log(protocolo, desplegable, cierreCovid);
 
 protocolo.addEventListener("click",() => {
     desplegable.style.display = "block";
+})
+cierreCovid.addEventListener("click",()=>{
+    desplegable.style.display = "none";
 })
